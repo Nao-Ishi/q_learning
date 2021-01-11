48546e8ed2fbd100d89f06054bd088b9fc655588
Nao_Ishi <Enushi0801@gmail.com>
2021-01-12 01:16:39 +0900

    done

--- scripts/goal_pose_logger.py
+++ scripts/goal_pose_logger.py
@@ -4,17 +4,21 @@ import rospy
 from nav_msgs.msg import Odometry
 from aruco_msgs.msg import MarkerArray
 
+global pose_log
+
 def odomCb(mag):
-    print mag.pose.pose
+    global pose_log
+    pose_log = mag.pose.pose
 
 def idCb(data):
+    global pose_log
     log = str(data.markers[0]).split()
-    print log[10],log[11]
+    print log[10],log[11], pose_log
 
 def pose_logger():
     rospy.init_node('goal_pose_logger', anonymous=True)
 
-    #rospy.Subscriber("odom",Odometry,odomCb)
+    rospy.Subscriber("odom",Odometry,odomCb)
     rospy.Subscriber("target_id",MarkerArray,idCb)
     rospy.spin()
 
