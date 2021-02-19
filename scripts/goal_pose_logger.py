a8c39475d634c5f7dbc6466cfe08c5931826bdc2
Nao-Ishi <44700085+Nao-Ishi@users.noreply.github.com>
2021-02-19 13:45:16 +0900

    Add files via upload

--- scripts/goal_pose_logger.py
+++ scripts/goal_pose_logger.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 
 import rospy
+import tf
 from nav_msgs.msg import Odometry
 from aruco_msgs.msg import MarkerArray
 
+global pose_log
+
 def odomCb(mag):
-    print mag.pose.pose
+    global pose_log
+    pose_log_x = str(round(mag.pose.pose.position.x,2))
+    pose_log_y = str(round(mag.pose.pose.position.y,2))
+    angs = tf.transformations.euler_from_quaternion((mag.pose.pose.orientation.x,mag.pose.pose.orientation.y,mag.pose.pose.orientation.z,mag.pose.pose.orientation.w))
+    pose_log = pose_log_x + ',' + pose_log_y + ',' + str(round(angs[2],2))
 
 def idCb(data):
+    global pose_log
     log = str(data.markers[0]).split()
-    print log[10],log[11]
+    print log[10],log[11],'writed',pose_log
+    path = 'data/' + str(log[11]) + '.txt'
+    with open(path,mode='a') as f:
+        f.write(str(pose_log))
+        f.write('\n')
+
+
 
 def pose_logger():
     rospy.init_node('goal_pose_logger', anonymous=True)
 
-    #rospy.Subscriber("odom",Odometry,odomCb)
+    rospy.Subscriber("odom",Odometry,odomCb)
     rospy.Subscriber("target_id",MarkerArray,idCb)
     rospy.spin()
 
---
+++ scripts/send_state.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#Fight!
+import rospy 
+import rospkg 
+import random
+import tf
+from gazebo_msgs.msg import ModelState 
+from gazebo_msgs.srv import SetModelState
+
+def state_sender():
+    rospy.init_node('state_sender')
+    r = rospy.Rate(10)
+    state_msg = ModelState()
+    state_msg.model_name = 'blue_bot'
+    state_msg.pose.position.x = 0.0
+    state_msg.pose.position.y = 3.0
+    state_msg.pose.position.z = 0.5
+    state_msg.pose.orientation.x = 0
+    state_msg.pose.orientation.y = 0
+    state_msg.pose.orientation.z = 0
+    state_msg.pose.orientation.w = 0
+    state_msg.reference_frame = 'world'
+    rospy.wait_for_service('/gazebo/set_model_state')
+    try:
+        set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+        resp = set_state( state_msg )
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
+
+    while not rospy.is_shutdown():
+        pose_x = random.uniform(-2,2)#area size x
+        pose_y = random.uniform(-2,2)#area size y
+        while abs(pose_x) + abs(pose_y) > 1.6:
+            pose_x = random.uniform(-2,2)
+            pose_y = random.uniform(-2,2)
+        q = tf.transformations.quaternion_from_euler(0.0, 0.0, round(random.uniform(-3.14159,3.14159),1))
+
+        state_msg = ModelState()
+        state_msg.model_name = 'red_bot'
+        state_msg.pose.position.x = round(pose_x,1)
+        state_msg.pose.position.y = round(pose_y,1)
+        state_msg.pose.position.z = 0.0
+        state_msg.pose.orientation.x = q[0]
+        state_msg.pose.orientation.y = q[1]
+        state_msg.pose.orientation.z = q[2]
+        state_msg.pose.orientation.w = q[3]
+        state_msg.reference_frame = 'world'
+        rospy.wait_for_service('/gazebo/set_model_state')
+        try:
+            set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            resp = set_state( state_msg )
+        except rospy.ServiceException, e:
+            print "Service call failed: %s" % e
+
+        r.sleep()
+
+   
+    
+if __name__ == '__main__':
+    try:
+        state_sender()
+    except rospy.ROSInterruptException:
+        pass
